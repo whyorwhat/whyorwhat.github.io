8e12ef8a39736551976a4bd7e3843d2f8bf498c3
simonecotardo <simo.cotardo@gmail.com>
2024-11-18 13:48:15 +0100

    first vue commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import WelcomeLanding from './components/WelcomeLanding.vue'
+</script>
+
+<template>
+  <main>
+    <WelcomeLanding />
+  </main>
+</template>
+
+<style scoped>
+body {
+  margin: 0;
+  padding: 0;
+}
+</style>
