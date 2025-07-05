502a39a7632c3f63ad800a3cc63f17c00aa91570
simonecotardo <simo.cotardo@gmail.com>
2025-07-05 19:01:54 +0200

    fixed not runnable direct links through HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { BrowserRouter } from "react-router-dom";
+import {BrowserRouter, HashRouter} from "react-router-dom";
 import AppContent from "@/global/AppContent.jsx";
 import ScrollToTop from "@/global/ScrollToTop.jsx";
 
 function App() {
   return (
-      <BrowserRouter>
+      <HashRouter>
           {/* side-effect prima di tutto */}
           <ScrollToTop />
 
         <AppContent />
-      </BrowserRouter>
+      </HashRouter>
   );
 }
 
